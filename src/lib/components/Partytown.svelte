d7501e8e35e3233e33ff6dce17de38a03a7fec43
nurliman <nurlimandiara@gmail.com>
2023-09-23 23:29:04 +0700

    use vercel rewrites for reverse proxy

--- src/lib/components/Partytown.svelte
+++ src/lib/components/Partytown.svelte
@@ -9,16 +9,16 @@
         resolveUrl: function (url, location, type) {
             if (type === "script") {
                 const pMap = {
-                    "www.google-analytics.com": "/google-analytics",
-                    "www.googleadservices.com": "/google-ad-services",
-                    "www.googletagmanager.com": "/google-tag-manager",
-                    "googleads.g.doubleclick.net": "/googleads-g-doubleclick",
+                    "www.google-analytics.com": "/proxy/google-analytics",
+                    "www.googleadservices.com": "/proxy/google-ad-services",
+                    "www.googletagmanager.com": "/proxy/google-tag-manager",
+                    "googleads.g.doubleclick.net": "/proxy/googleads-g-doubleclick",
                 };
 
                 if (!pMap[url.hostname]) return url;
 
                 const pUrl = new URL(url);
-                pUrl.hostname = "proxy.knitto.co.id";
+                pUrl.hostname = "turbopic.vercel.app";
                 pUrl.pathname = (pMap[url.hostname] || "") + url.pathname;
                 return pUrl;
             }
