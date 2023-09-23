ff37869fa71edfc34fc6af279d8ca92b98cc5514
nurliman <nurlimandiara@gmail.com>
2023-09-23 23:51:16 +0700

    format

--- src/lib/components/GoogleAnalytics.svelte
+++ src/lib/components/GoogleAnalytics.svelte
@@ -1,9 +1,6 @@
 <!-- TODO: use environment variables for google tag manager -->
 <svelte:head>
-  <script
-    type="text/partytown"
-    src="https://www.googletagmanager.com/gtag/js?id=G-FK86TVQGTV"
-  ></script>
+  <script type="text/partytown" src="https://www.googletagmanager.com/gtag/js?id=G-FK86TVQGTV"></script>
   <script type="text/partytown">
     window.dataLayer = window.dataLayer || [];
     function gtag() {
--- src/lib/components/Partytown.svelte
+++ src/lib/components/Partytown.svelte
@@ -1,32 +1,30 @@
 <script>
-      import { partytownSnippet } from "@builder.io/partytown/integration";
+  import { partytownSnippet } from "@builder.io/partytown/integration";
 </script>
-
 <svelte:head>
   <script>
     partytown = {
-        forward: ["dataLayer.push"],
-        resolveUrl: function (url, location, type) {
-            if (type === "script") {
-                const pMap = {
-                    "www.google-analytics.com": "/proxy/google-analytics",
-                    "www.googleadservices.com": "/proxy/google-ad-services",
-                    "www.googletagmanager.com": "/proxy/google-tag-manager",
-                    "googleads.g.doubleclick.net": "/proxy/googleads-g-doubleclick",
-                };
+      forward: ["dataLayer.push"],
+      resolveUrl: function (url, location, type) {
+        if (type === "script") {
+          const pMap = {
+            "www.google-analytics.com": "/proxy/google-analytics",
+            "www.googleadservices.com": "/proxy/google-ad-services",
+            "www.googletagmanager.com": "/proxy/google-tag-manager",
+            "googleads.g.doubleclick.net": "/proxy/googleads-g-doubleclick",
+          };
 
-                if (!pMap[url.hostname]) return url;
+          if (!pMap[url.hostname]) return url;
 
-                const pUrl = new URL(url);
-                pUrl.hostname = "turbopic.vercel.app";
-                pUrl.pathname = (pMap[url.hostname] || "") + url.pathname;
-                return pUrl;
-            }
+          const pUrl = new URL(url);
+          pUrl.hostname = "turbopic.vercel.app";
+          pUrl.pathname = (pMap[url.hostname] || "") + url.pathname;
+          return pUrl;
+        }
 
-            return url;
-        },
+        return url;
+      },
     };
   </script>
-
   {@html "<script>" + partytownSnippet() + "</script>"}
 </svelte:head>
